b2cc091d2b32fad227aab266cebd69b263d441b9
Mark Roberts <m4rrk@hotmail.com>
2025-03-22 07:29:40 +0000

    Initial commit for WebSocket server

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { Route, HashRouter as Router, Routes } from 'react-router-dom';
 import './App.css';
 import TokenDashboard from './components/TokenDashboard';
 import TokenDetailPage from './components/TokenDetailPage';
